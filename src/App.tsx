cbedd1129eff586da6911b76dddaafe92598bbd5
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2025-01-29 10:37:53 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-
+import Home from './pages/home/Home'
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <Home />
+    )
 }
+
+export default App
