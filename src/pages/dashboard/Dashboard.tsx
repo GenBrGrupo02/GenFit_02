0b97cd50e7ae328bd8df9da2c36683b9e3b9d498
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2025-01-30 19:19:24 -0300

    Update Dashboard.tsx

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -9,7 +9,6 @@ import {
 } from "@phosphor-icons/react";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import ListaExercicios from "../../components/exercicios/ListaExercicios";
 
 function Dashboard() {
   const navigate = useNavigate();
