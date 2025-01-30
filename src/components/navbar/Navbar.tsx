5b9fc94932d5aeb3380388d92f02bfdceb7f6626
Michele Bueno <contato.michelebueno@gmail.com>
2025-01-30 10:39:49 -0300

    Atualizar navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
 
@@ -17,28 +18,27 @@ function Navbar() {
     return (
         <div className="w-full flex justify-center py-4 bg-base-100 text-white ">
             <div className="container flex justify-between text-lg ">
-                <span className="font-title text-yellow-500">GENFIT</span>
-                <div>
-                    
-                <img
-                                src="https://i.imgur.com/UoA4cEN.png"
-                                alt="Imagem da Página Home"
-                                style={{
-                                    width: "8vh",
-                                    height: "auto",
-                                }}
-                            />
+                <div className="mr-auto">
+
+                    <img
+                        src="https://i.imgur.com/UoA4cEN.png"
+                        alt="Imagem da Página Home"
+                        style={{
+                            width: "8vh",
+                            height: "auto",
+                        }}
+                    />
                 </div>
-                
+
 
                 <div className="flex gap-8 font-body  justify-end items-center ">
-
-                    <button className="btn btn-outline">PERFIL</button>
-                    <button className="btn btn-outline btn-primary">EXERCÍCIOS</button>
-                    <button className="btn btn-outline btn-secondary">OBJETIVOS</button>
+                    <Link to="/login" className="hover:underline">
+                        <button className="btn btn-outline">LOGIN</button>
+                    </Link>
+                    <Link to="/sobrenos" className="hover:underline">
                     <button className="btn btn-outline btn-warning">SOBRE NÓS</button>
-                    <button className="btn btn-outline btn-accent">SAIR</button>
-              
+                    </Link>
+                   
                 </div>
             </div>
         </div>
