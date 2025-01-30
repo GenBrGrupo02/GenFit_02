fa8287876e434c552a254ae3fb7c1c0d0df6bed5
StPVieira <pedrohq.vieira168@gmail.com>
2025-01-30 13:13:40 -0300

    feat: navbar updated with login context

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+  const { usuario, handleLogout } = useContext(AuthContext); 
+
   return (
     <>
-      <div className="w-full flex justify-center py-4 bg-base-100 text-white ">
-        <div className="container flex justify-between text-lg ">
+      <div className="w-full flex justify-center py-4 bg-base-100 text-white">
+        <div className="container flex justify-between text-lg">
           <div className="mr-auto">
             <img
               src="https://i.imgur.com/UoA4cEN.png"
@@ -16,10 +20,19 @@ function Navbar() {
             />
           </div>
 
-          <div className="flex gap-8 font-body  justify-end items-center ">
-            <Link to="/login" className="hover:underline">
-              <button className="btn btn-outline">LOGIN</button>
-            </Link>
+          <div className="flex gap-8 font-body justify-end items-center">
+            {!usuario.token ? (
+              <Link to="/login" className="hover:underline">
+                <button className="btn btn-outline">LOGIN</button>
+              </Link>
+            ) : (
+              <>
+                <span className="text-lg">{usuario.nome}</span>
+                <button className="btn btn-outline" onClick={handleLogout}>
+                  LOGOUT
+                </button>
+              </>
+            )}
             <Link to="/sobrenos" className="hover:underline">
               <button className="btn btn-outline btn-warning">SOBRE NÓS</button>
             </Link>
